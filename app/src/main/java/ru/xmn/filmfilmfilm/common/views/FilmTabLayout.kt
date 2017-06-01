7aa27449a335bc718fa238fa73daa9947ac18c0d
konorev <sglvzom@gmail.com>
2017-06-01 11:53:00 +0300

    fixes

--- app/src/main/java/ru/xmn/filmfilmfilm/common/views/FilmTabLayout.kt
+++ app/src/main/java/ru/xmn/filmfilmfilm/common/views/FilmTabLayout.kt
@@ -2,10 +2,9 @@ package ru.xmn.filmfilmfilm.common.views
 
 import android.content.Context
 import android.support.design.widget.TabLayout
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.ViewPager
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import ru.xmn.filmfilmfilm.common.abs
@@ -16,7 +15,7 @@ class FilmTabLayout(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
 
     constructor (context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
 
-    val textViews = mutableListOf<FilmTabTextView>()
+    private val textViews = mutableListOf<FilmTabTextView>()
 
     override fun setupWithViewPager(viewPager: ViewPager?) {
         super.setupWithViewPager(viewPager)
@@ -27,29 +26,53 @@ class FilmTabLayout(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
             view.id = android.R.id.text1
             tab?.customView = view
         }
+
         viewPager?.setPageTransformer(false, object : ViewPager.PageTransformer {
             override fun transformPage(page: View?, position: Float) {
                 // Get page index from tag
-                val pagePosition = page?.getTag() as Int
-
-                when {
-                    position <= -1.0f || position >= 1.0f -> textViews[pagePosition].transformTo(0F)
-                    else -> textViews[pagePosition].transformTo(1 - position.abs())
+                val pagePosition = page?.getTag() as? Int
+                if (pagePosition == null) {
+                    Log.v("FilmTabLayout", "working only with views in viewpager with Int tags")
+                    return
                 }
 
+                when {
+                    position <= -1.0f || position >= 1.0f -> textViews[pagePosition].scaleText(0F)
+                    else -> textViews[pagePosition].scaleText(1 - position.abs())
+                }
             }
 
         })
     }
 }
 
-class FilmTabTextView(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : TextView(context, attrs, defStyleAttr) {
+
+private class FilmTabTextView(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : TextView(context, attrs, defStyleAttr) {
+
+    var scaleCoeff = 2F
+    private var commonTextSize: Float = 0F
+    private var selectedTextSize: Float = 0F
+
+    fun changeTextSize(size: Float) {
+        super.setTextSize(size)
+        commonTextSize = size
+        selectedTextSize = size * scaleCoeff
+    }
 
     constructor (context: Context) : this(context, null)
 
     constructor (context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
 
-    fun transformTo(positionOffset: Float) {
-        textSize = 15 + 5 * positionOffset
+    init {
+        changeTextSize(15F)
+    }
+
+    fun scaleText(positionOffset: Float) {
+        textSize = commonTextSize + (selectedTextSize - commonTextSize) * positionOffset
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
     }
 }
